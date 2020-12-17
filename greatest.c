5e1a78844acd2d026361236dce4ae25fbfa5cf37
Ashutosh <66676728+Ashutosh-ops@users.noreply.github.com>
2020-12-17 15:17:02 +0530

    Task_3

--- greatest.c
+++ greatest.c
@@ -2,9 +2,27 @@
 
 int main(int argc, char const *argv[])
 {
-    int a,b,c,r;
-    scanf("%d%d%d",&a,&b,&c);
-    r=(a>b)?(a>c)?a:c:(b>c)?b:c;
-    printf("\nGreatest number is: %d\n",r);
+    int a, b, c, d, r;
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+    r = (a > b) ? (a > c) ? a : c : (b > c) ? b : c;
+    if (d >= r)
+    {
+        printf("\nGreatest number is: %d\n", d);
+    }
+    else
+    {
+        if (a == r)
+        {
+            printf("\na is Greatest number is: %d\n", a);
+        }
+        else if (r == b)
+        {
+            printf("\nb is Greatest number is: %d\n", b);
+        }
+        else
+        {
+            printf("\nc is Greatest number is: %d\n", c);
+        }
+    }
     return 0;
 }
